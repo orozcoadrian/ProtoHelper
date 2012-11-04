915563ff57d4dbe04ceab933825b3709bb25773b
mfullen <mike@mikefullen.com>
2012-11-03 23:37:44 -0400

    +swing ui

---
+++ UI/src/main/java/com/pwf/ui/ProtoHelperSwingUI.java
@@ -0,0 +1,124 @@
+package com.pwf.ui;
+
+import com.google.protobuf.Message;
+import com.pwf.core.Engine;
+import com.pwf.core.NoLoadedMessagesException;
+import com.pwf.plugin.PluginInformation;
+import com.pwf.plugin.PluginManagerLite;
+import com.pwf.protohelper.platform.UserInterfacePHP;
+import java.awt.BorderLayout;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+/**
+ *
+ * @author mfullen
+ */
+public class ProtoHelperSwingUI implements UserInterfacePHP
+{
+    private PluginManagerLite manager = null;
+    private final PluginInformation pluginInformation = new PluginInformationImpl();
+    private Engine engine = null;
+
+    @Override
+    public void startInterface(Engine coreEngine)
+    {
+        try
+        {
+            this.engine = coreEngine;
+
+            boolean findMessagesOnClassth = engine.findMessagesOnClasspath();
+            Collection<Message.Builder> protoBuilders = engine.getProtoBuilders();
+
+
+            DefaultListModel listModel = new DefaultListModel();
+            for (Message.Builder builder : protoBuilders)
+            {
+                listModel.addElement(builder);
+            }
+
+
+            JList list = new JList(listModel);
+
+            JScrollPane scrollPane = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+            JPanel panel = new JPanel(new BorderLayout());
+            panel.add(scrollPane, BorderLayout.CENTER);
+
+            JFrame frame = new JFrame("Protos");
+            frame.add(panel);
+            frame.pack();
+            frame.setVisible(true);
+        }
+        catch (NoLoadedMessagesException ex)
+        {
+            Logger.getLogger(ProtoHelperSwingUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void onLoaded(PluginManagerLite pluginManager)
+    {
+        this.manager = pluginManager;
+    }
+
+    @Override
+    public void onActivated()
+    {
+    }
+
+    @Override
+    public void onDeactivated()
+    {
+    }
+
+    @Override
+    public PluginInformation getPluginInformation()
+    {
+        return this.pluginInformation;
+    }
+
+    protected static class PluginInformationImpl implements PluginInformation
+    {
+        public PluginInformationImpl()
+        {
+        }
+
+        @Override
+        public String getName()
+        {
+            return "ProtoHelper Console UI Plugin";
+        }
+
+        @Override
+        public String getVersion()
+        {
+            return "1.0.0";
+        }
+
+        @Override
+        public String getProvider()
+        {
+            return PluginInformation.DEFAULT_PROVIDER;
+        }
+
+        @Override
+        public String getIdentifier()
+        {
+            return ProtoHelperSwingUI.class.getName();
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        PluginInformation pluginInfo = this.getPluginInformation();
+        return String.format("%s (%s) %s © %s", pluginInfo.getName(), pluginInfo.getIdentifier(), pluginInfo.getVersion(),
+                pluginInfo.getProvider());
+    }
+}
